bebc3063ab6c3c37540612487a130f6d94247c73
HSbF5HSO3F <jinghaoyu1030@qq.com>
2024-10-10 21:49:04 +0800

    核心代码：添加目前已有函数

--- Scripts/AzurLaneCoreScript.lua
+++ Scripts/AzurLaneCoreScript.lua
@@ -2,3 +2,159 @@
 -- Author: jjj
 -- DateCreated: 2024/10/10 21:10:53
 --------------------------------------------------------------
+--建立代码框架
+AzurLaneCore = {}
+
+--||====================GamePlay, UI======================||--
+--通用函数
+
+--判断领袖，玩家不为指定领袖类型则返回false (GamePlay, UI)
+function AzurLaneCore.CheckLeader(playerID, leaderType)
+    local pPlayerConfig = playerID and PlayerConfigurations[playerID]
+    return pPlayerConfig and pPlayerConfig:GetLeaderTypeName() == leaderType
+end
+
+--判断文明，玩家文明不为指定文明类型则返回false (GamePlay, UI)
+function AzurLaneCore.CheckCivilization(playerID, civilizationType)
+    local pPlayerConfig = playerID and PlayerConfigurations[playerID]
+    return pPlayerConfig and pPlayerConfig:GetCivilizationTypeName() == civilizationType
+end
+
+--数字四舍五入处理 (GamePlay, UI)
+function AzurLaneCore.Round(num)
+    return math.floor((num + 0.05) * 10) / 10
+end
+
+--随机数生成器，范围为[1,num+1] (GamePlay, UI)
+function AzurLaneCore.tableRandom(num)
+    return Game.GetRandNum and (Game.GetRandNum(num) + 1) or 1
+end
+
+--将输入的数字按照当前游戏速度进行修正 (GamePlay, UI)
+function AzurLaneCore:ModifyBySpeed(num)
+    local gameSpeed = GameInfo.GameSpeeds[GameConfiguration.GetGameSpeedType()]
+    if gameSpeed then num = self.Round(num * gameSpeed.CostMultiplier / 100) end
+    return num
+end
+
+--检查table中是否有指定元素 (GamePlay, UI)
+function AzurLaneCore.include(table, element)
+    for _, value in pairs(table) do
+        if value == element then
+            return true
+        end
+    end
+    return false
+end
+
+--检查科技或者市政是否拥有提升 (GamePlay, UI)
+function AzurLaneCore.HasBoost(techOrCivic)
+    for boost in GameInfo.Boosts() do
+        if techOrCivic == boost.TechnologyType or techOrCivic == boost.CivicType then
+            return true
+        end
+    end
+    return false
+end
+
+--获得玩家游戏进度。返回为百分比，需除以100 (GamePlay, UI)
+function AzurLaneCore:GetPlayerProgress(playerID)
+    local pPlayer = Players[playerID]
+    if pPlayer == nil then return 0 end
+    local playerTech = pPlayer:GetTechs()
+    local techNum, techedNum = 0, 0
+    for row in GameInfo.Technologies() do
+        techNum = techNum + 1
+        if playerTech:HasTech(row.Index) then
+            techedNum = (techedNum or 0) + 1
+        end
+    end
+    local playerCulture = pPlayer:GetCulture()
+    local civicNum, civicedNum = 0, 0
+    for row in GameInfo.Civics() do
+        civicNum = civicNum + 1
+        if playerCulture:HasCivic(row.Index) then
+            civicedNum = (civicedNum or 0) + 1
+        end
+    end
+    local civicProgress = civicNum ~= 0 and civicedNum / civicNum or 0
+    local techProgress = techNum ~= 0 and techedNum / techNum or 0
+    return self.Round(100 * math.max(techProgress, civicProgress))
+end
+
+--||=====================GamePlay=======================||--
+--这些函数只可在GamePlay环境下使用
+
+--玩家获得随机数量的尤里卡 (GamePlay)
+function AzurLaneCore:GetRandomTechBoosts(playerID, iSource, num)
+    local pPlayer = Players[playerID]
+    local EraIndex = 1
+    local playerTech = pPlayer:GetTechs()
+    local limit = num or 1
+    while limit > 0 do
+        local EraType = nil
+        for era in GameInfo.Eras() do
+            if era.ChronologyIndex == EraIndex then
+                EraType = era.EraType
+                break
+            end
+        end
+        if EraType then
+            local techlist = {}
+            for row in GameInfo.Technologies() do
+                if not (playerTech:HasTech(row.Index) or
+                        playerTech:HasBoostBeenTriggered(row.Index) or
+                        self.HasBoost(row.TechnologyType))
+                    and row.EraType == EraType then
+                    table.insert(techlist, row.Index)
+                end
+            end
+            if #techlist > 0 then
+                local iTech = techlist[self.tableRandom(#techlist)]
+                playerTech:TriggerBoost(iTech, iSource)
+                limit = limit - 1
+            else
+                EraIndex = (EraIndex or 0) + 1
+            end
+        else
+            break
+        end
+    end
+end
+
+--玩家获得随机数量的鼓舞 (GamePlay)
+function AzurLaneCore:GetRandomCivicBoosts(playerID, iSource, num)
+    local pPlayer = Players[playerID]
+    local EraIndex = 1
+    local playerCulture = pPlayer:GetCulture()
+    local limit = num or 1
+    while limit > 0 do
+        local EraType = nil
+        for era in GameInfo.Eras() do
+            if era.ChronologyIndex == EraIndex then
+                EraType = era.EraType
+                break
+            end
+        end
+        if EraType then
+            local civiclist = {}
+            for row in GameInfo.Civics() do
+                if not (playerCulture:HasCivic(row.Index) or
+                        playerCulture:HasBoostBeenTriggered(row.Index) or
+                        self.HasBoost(row.CivicType))
+                    and row.EraType == EraType then
+                    table.insert(civiclist, row.Index)
+                end
+            end
+            if #civiclist > 0 then
+                local iCivic = civiclist[self.tableRandom(#civiclist)]
+                playerCulture:TriggerBoost(iCivic, iSource)
+                limit = limit - 1
+            else
+                EraIndex = (EraIndex or 0) + 1
+            end
+        else
+            break
+        end
+    end
+end
