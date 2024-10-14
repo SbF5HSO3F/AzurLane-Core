541ca4e312ba72591a15d2dfccd72adbf03dc869
HSbF5HSO3F <jinghaoyu1030@qq.com>
2024-10-14 09:52:50 +0800

    核心代码：优化A*寻路算法，增加玩家ID参数，新增路径构建及节点扩展函数，改进单位比较及检查战斗力功能，调整部分逻辑和注释。

--- Scripts/AzurLaneCoreScript.lua
+++ Scripts/AzurLaneCoreScript.lua
@@ -119,27 +119,27 @@ function AzurLaneCore:GetAStarNode(sIndex, eIndex, parent)
     }; return node
 end
 
---检测单元格是否满足A*算法的条件 (UI)
-function AzurLaneCore.CheckPlot(pPlot)
+--检测单元格是否满足A*算法的条件 (GamePlay, UI)
+function AzurLaneCore.CheckPlot(pPlot, playerID)
     --can pass?
     if pPlot:IsImpassable() then return false end
     --is lake?
     if pPlot:IsLake() then return false end
     --is viasble?
-    local visibility = PlayersVisibility[Game.GetLocalPlayer()]
+    local visibility = PlayersVisibility[playerID]
     if not visibility:IsRevealed(pPlot:GetX(), pPlot:GetY()) then
         return false
     end; return true
 end
 
 --获取A*算法的路径点相邻单元格 (GamePlay, UI)
-function AzurLaneCore:GetNodeNeighbor(node, eIndex)
+function AzurLaneCore:GetNodeNeighbor(node, eIndex, playerID)
     local neighbors = {}
     if node then
         local pPlot = Map.GetPlotByIndex(node.Index)
         local x, y = pPlot:GetX(), pPlot:GetY()
         for _, plot in ipairs(Map.GetAdjacentPlots(x, y)) do
-            if self.CheckPlot(plot) then
+            if self.CheckPlot(plot, playerID) then
                 table.insert(neighbors, self:GetAStarNode(plot:GetIndex(), eIndex, node))
             end
         end
@@ -152,12 +152,101 @@ function AzurLaneCore.GetPlotG(index)
     return pPlot and (pPlot:GetRouteType() ~= -1 and 1 or (pPlot:IsWater() and 20 or 5)) or 5
 end
 
---A*寻路算法 (GamePlay, UI)
-function AzurLaneCore:AStar(startX, startY, endX, endY)
+--寻找F为最小值的节点 (GamePlay, UI)
+function AzurLaneCore.GetMinFNode(openTable)
+    local currentIndex = 1
+    local current = openTable[currentIndex]
+    for i = 2, #openTable do
+        local currentNode = openTable[i]
+        if currentNode.F < current.F or (currentNode.F == current.F and currentNode.H < current.H) then
+            current = currentNode
+            currentIndex = i
+        end
+    end
+    return table.remove(openTable, currentIndex)
+end
+
+--获取相遇节点，没有相遇则返回nil (GamePlay, UI)
+function AzurLaneCore.GetMeetNode(index, openTable)
+    local meetNode = nil
+    for _, node in ipairs(openTable) do
+        if node.Index == index then
+            meetNode = node
+            break
+        end
+    end
+    return meetNode
+end
+
+--构建路径 (GamePlay, UI)
+function AzurLaneCore.BuildPath(path, node, meetNode)
+    local pathFromStart = {}
+    local pathFromEnd = {}
+    --获得从起点到相遇点的路径
+    while node do
+        table.insert(pathFromStart, node.Index)
+        node = node.Parent
+    end
+    --获得从终点到相遇点的路径
+    node = meetNode.Parent
+    while node do
+        table.insert(pathFromEnd, node.Index)
+        node = node.Parent
+    end
+    --倒转添加从起点到相遇点的路径
+    for i = #pathFromStart, 1, -1 do
+        table.insert(path, pathFromStart[i])
+    end
+    --添加从终点到相遇点的路径
+    for i = 1, #pathFromEnd do
+        table.insert(path, pathFromEnd[i])
+    end
+end
+
+--扩展相邻节点 (GamePlay, UI)
+function AzurLaneCore:ExpandNeighbors(node, openTable, closeSet, eIndex, playerID)
+    --获取相邻节点
+    for _, neighbor in ipairs(self:GetNodeNeighbor(node, eIndex, playerID)) do
+        local neighborIndex = neighbor.Index
+        --如果节点不在关闭集合中
+        if not closeSet[neighborIndex] then
+            --获取 G/H/F 值
+            local G = neighbor.G
+            local H = neighbor.H
+            local F = neighbor.F
+            --如果该节点位于开放表格中，更新 G/H/F 值
+            local isInOpenTable = false
+            for _, openNode in ipairs(openTable) do
+                if openNode.Index == neighborIndex then
+                    isInOpenTable = true
+                    if G < openNode.G then
+                        --update the G/H/F
+                        openNode.G = G
+                        openNode.H = H
+                        openNode.F = F
+                        openNode.Parent = node
+                    end
+                    break
+                end
+            end
+            --否则加入开放表格
+            if not isInOpenTable then
+                neighbor.G = G
+                neighbor.H = H
+                neighbor.F = F
+                neighbor.Parent = node
+                table.insert(openTable, neighbor)
+            end
+        end
+    end
+end
+
+--A*寻路算法，双向 (GamePlay, UI)
+function AzurLaneCore:AStar(startX, startY, endX, endY, playerID)
     --返回的结果
     local results = {}
-    --起点与终点不为nil
-    if not (startX and startY and endX and endY) then return results end
+    --起点与终点，还有玩家ID不为nil
+    if not (startX and startY and endX and endY and playerID) then return results end
     --获得起点单元格和终点单元格
     local s_plot = Map.GetPlot(startX, startY)
     local e_plot = Map.GetPlot(endX, endY)
@@ -165,73 +254,78 @@ function AzurLaneCore:AStar(startX, startY, endX, endY)
     if not (s_plot and e_plot) or s_plot == e_plot then return results end
     --获得起点和终点单元格的索引
     local s_index, e_index = s_plot:GetIndex(), e_plot:GetIndex()
-    --初始化开始表格和关闭集合
-    local openTable, closeSet = {}, {}
-    --开始表格加入起点节点
-    table.insert(openTable, self:GetAStarNode(s_index, e_index, nil))
+    --初始化起点开始表格和关闭集合
+    local openTableS, openTableE = {}, {}
+    --初始化终点开始表格和关闭集合
+    local closeSetS, closeSetE = {}, {}
+    --起点开始表格加入起点节点
+    table.insert(openTableS, self:GetAStarNode(s_index, e_index, nil))
+    --终点开始表格加入终点节点
+    table.insert(openTableE, self:GetAStarNode(e_index, s_index, nil))
+    --中转节点和相遇节点
+    local middleNode, meetNode = nil, nil
     --开始寻路
-    while #openTable > 0 do
+    while #openTableS > 0 and #openTableE > 0 do
         --获得F值最小的节点
-        local currentIndex = 1
-        local current = openTable[currentIndex]
-        for i = 2, #openTable do
-            local currentNode = openTable[i]
-            if currentNode.F < current.F or (currentNode.F == current.F and currentNode.H < current.H) then
-                current = currentNode
-                currentIndex = i
-            end
-        end
+        middleNode = self.GetMinFNode(openTableS)
+        meetNode = self.GetMeetNode(middleNode.Index, openTableE)
 
-        table.remove(openTable, currentIndex)
-
-        --达到终点，构造路径
-        if current.Index == e_index then
-            while current do
-                table.insert(results, current.Index)
-                current = current.Parent
-            end
+        --如果中转节点和相遇节点都存在，则找到路径并返回
+        if middleNode and meetNode then
+            --build the path
+            self.BuildPath(results, middleNode, meetNode)
             return results
         end
 
-        closeSet[current.Index] = true
+        closeSetS[middleNode.Index] = true
+        --扩展相邻节点
+        self:ExpandNeighbors(middleNode, openTableS, closeSetS, e_index, playerID)
 
-        --获取节点相邻单元格
-        for _, neighbor in ipairs(self:GetNodeNeighbor(current, e_index)) do
-            local neighborIndex = neighbor.Index
-            --如果不在关闭集合，进行处理
-            if not closeSet[neighborIndex] then
-                --获取 G/H/F 值
-                local G = neighbor.G
-                local H = neighbor.H
-                local F = neighbor.F
-                --如果在开放表格中，更新G值
-                local isInOpenTable = false
-                for _, openNode in ipairs(openTable) do
-                    if openNode.Index == neighborIndex then
-                        isInOpenTable = true
-                        if G < openNode.G then
-                            --更新 G/H/F 值
-                            openNode.G = G
-                            openNode.H = H
-                            openNode.F = F
-                            openNode.Parent = current
-                        end
-                        break
-                    end
-                end
+        --获得F值最小的节点
+        middleNode = self.GetMinFNode(openTableE)
+        meetNode = self.GetMeetNode(middleNode.Index, openTableS)
 
-                --如果不在开放表格中，加入开放表格
-                if not isInOpenTable then
-                    neighbor.G = G
-                    neighbor.H = H
-                    neighbor.F = F
-                    neighbor.Parent = current
-                    table.insert(openTable, neighbor)
-                end
-            end
+        --如果中转节点和相遇节点都存在，则找到路径并返回
+        if middleNode and meetNode then
+            --build the path
+            self.BuildPath(results, middleNode, meetNode)
+            return results
         end
+
+        closeSetE[middleNode.Index] = true
+        --扩展相邻节点
+        self:ExpandNeighbors(middleNode, openTableE, closeSetE, e_index, playerID)
     end
     return results
+end
+
+--比较单位，如果单位2强度高于单位1则返回true (GamePlay, UI)
+function AzurLaneCore.CompareUnitDef(unit1Def, unit2Def)
+    if unit1Def == nil then return true end
+    if unit2Def == nil then return false end
+
+    if unit1Def.Combat ~= unit2Def.Combat then
+        return unit1Def.Combat < unit2Def.Combat
+    end
+
+    if unit1Def.RangedCombat ~= unit2Def.RangedCombat then
+        return unit1Def.RangedCombat < unit2Def.RangedCombat
+    end
+
+    if unit1Def.Bombard ~= unit2Def.Bombard then
+        return unit1Def.Bombard < unit2Def.Bombard
+    end
+
+    if unit1Def.Range ~= unit2Def.Range then
+        return unit1Def.Range < unit2Def.Range
+    end
+
+    return unit1Def.BaseMoves < unit2Def.BaseMoves
+end
+
+--检查单位是否拥有战斗力 (GamePlay, UI)
+function AzurLaneCore.HasStrength(unit)
+    return unit and (unit:GetCombat() > 0 or unit:GetRangedCombat() > 0 or unit:GetBombardCombat() > 0)
 end
 
 --||=====================GamePlay=======================||--
@@ -256,7 +350,7 @@ function AzurLaneCore:GetRandomTechBoosts(playerID, iSource, num)
             for row in GameInfo.Technologies() do
                 if not (playerTech:HasTech(row.Index) or
                         playerTech:HasBoostBeenTriggered(row.Index) or
-                        self.HasBoost(row.TechnologyType))
+                        not self.HasBoost(row.TechnologyType))
                     and row.EraType == EraType then
                     table.insert(techlist, row.Index)
                 end
@@ -293,7 +387,7 @@ function AzurLaneCore:GetRandomCivicBoosts(playerID, iSource, num)
             for row in GameInfo.Civics() do
                 if not (playerCulture:HasCivic(row.Index) or
                         playerCulture:HasBoostBeenTriggered(row.Index) or
-                        self.HasBoost(row.CivicType))
+                        not self.HasBoost(row.CivicType))
                     and row.EraType == EraType then
                     table.insert(civiclist, row.Index)
                 end
